26a58543b11849b8a11f6066b8ffe2197dd3d1a9
Velo <hartley.max@outlook.com>
2021-01-29 16:30:29 +0000

    bars animate in automatically and randomly

--- components/bars/bars.jsx
+++ components/bars/bars.jsx
@@ -1,17 +1,45 @@
 import { useState, useEffect } from 'react';
-import style from './bars.module.css';
+import styles from './bars.module.css';
 
 export default function Bars({ paused }) {
 	const [bars, setBars] = useState([]);
 
-	return (
-		<>
-			{bars}
-			<Bar />
-		</>
-	);
+	const createBarStyle = (random) => {
+		return {
+			animationDuration: `${5 - random}s`,
+			animationDelay: `${random}s`,
+			height: `${15 + Math.floor(random * 50)}px`,
+			right: `${150 * random - 50}vw`,
+		};
+	};
+
+	useEffect(() => {
+		const getBarID = () => Math.random() * Math.floor(Math.random() * 100 + 100);
+
+		// Adds a bar to state then removes onAnimation finishing
+		const addBar = () => {
+			const barID = getBarID();
+			const randomNumber = Math.random();
+
+			setBars((prevState) => [
+				...prevState,
+				<Bar style={createBarStyle(randomNumber)} key={barID} />,
+			]);
+			setTimeout(() => {
+				setBars((prevState) => prevState.filter((bar) => bar.key != barID));
+			}, 5000 - randomNumber * 1000 + randomNumber * 950);
+		};
+		const timeoutID = setTimeout(() => {
+			addBar();
+			addBar();
+			addBar();
+		}, Math.floor(Math.random() * 1500) + 400);
+		return () => clearTimeout(timeoutID);
+	}, [bars]);
+
+	return <>{bars}</>;
 }
 
-const Bar = () => {
-	return <div className={style.bar}></div>;
+const Bar = ({ style }) => {
+	return <div className={styles.bar} style={style}></div>;
 };
