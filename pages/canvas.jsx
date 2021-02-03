0e6c31477b7a455cee5ff3541fc5cc926a5237f1
Velo <hartley.max@outlook.com>
2021-02-03 22:28:28 +0000

    200! tiny tiny bouncing balls

--- pages/canvas.jsx
+++ pages/canvas.jsx
@@ -9,66 +9,89 @@ export default function Page() {
 	const [isPlaying, setIsPlaying] = useState(true);
 	const canvasRef = useRef(null);
 
+	class Ball {
+		constructor(ctx, width, height) {
+			this.x = Math.floor(Math.random() * 1000);
+			this.y = Math.floor(Math.random() * 1000);
+			this.ctx = ctx;
+		}
+		vx = 5;
+		vy = 2;
+		radius = 5;
+		color = '#161d6f';
+		draw() {
+			this.ctx.beginPath();
+			this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+			this.ctx.closePath();
+			this.ctx.fillStyle = this.color;
+			this.ctx.fill();
+		}
+	}
+
+	const createBalls = (volume, ctx) => {
+		let newBalls = [];
+		console.log(volume);
+		console.log(ctx);
+		for (let i = 0; i < volume; i++) {
+			newBalls[i] = new Ball(ctx, width, height);
+			console.log(newBalls[i].x);
+		}
+		return newBalls;
+	};
+
 	useEffect(() => {
 		const canvas = canvasRef.current;
 		const ctx = canvas.getContext('2d');
+		const balls = createBalls(200, ctx);
 		let raf;
-		const ball = {
-			x: 100,
-			y: 100,
-			vx: 5,
-			vy: 2,
-			radius: 25,
-			color: '#161d6f',
-			draw: function () {
-				ctx.beginPath();
-				ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-				ctx.closePath();
-				ctx.fillStyle = this.color;
-				ctx.fill();
-			},
-		};
+		console.log('useEffect');
+		console.log(balls);
 
 		function draw() {
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			ball.draw();
-			if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
-				ball.vy = -ball.vy;
-			}
-			if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
-				ball.vx = -ball.vx;
-			}
-			if (Math.abs(ball.vy) < 0.3 && canvas.height - ball.y < 30) {
-				ball.vy = 25;
-				if (Math.abs(ball.vx) < 0.1) {
-					Math.random() < 0.5 ? (ball.vx += 10) : (ball.vx -= 10);
+			for (let ball of balls) {
+				ball.draw();
+				if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
+					ball.vy = -ball.vy;
 				}
+				if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+					ball.vx = -ball.vx;
+				}
+				if (Math.abs(ball.vy) < 0.3 && canvas.height - ball.y < 30) {
+					ball.vy = 25;
+					if (Math.abs(ball.vx) < 0.1) {
+						Math.random() < 0.5 ? (ball.vx += 10) : (ball.vx -= 10);
+					}
+				}
+
+				ball.vy *= 0.99;
+				ball.vy += 0.2;
+				ball.vx *= 0.995;
+				ball.x += ball.vx;
+				ball.y += ball.vy;
 			}
 
-			ball.vy *= 0.99;
-			ball.vy += 0.2;
-			ball.vx *= 0.995;
-			ball.x += ball.vx;
-			ball.y += ball.vy;
 			raf = window.requestAnimationFrame(draw);
 		}
-
 		raf = window.requestAnimationFrame(draw);
 
-		ball.draw();
+		for (let ball of balls) {
+			ball.draw();
+		}
 
 		const onClick = ({ clientX, clientY }) => {
-			if (clientY > ball.y) {
-				ball.vy -= 20;
-			} else {
-				ball.vy += 20;
+			for (let ball of balls) {
+				if (clientY > ball.y) {
+					ball.vy -= 20;
+				} else {
+					ball.vy += 20;
+				}
+				if (clientX > ball.x) {
+					ball.vx -= 10;
+				} else {
+					ball.vx += 10;
+				}
 			}
-			if (clientX > ball.x) {
-				ball.vx -= 10;
-			} else {
-				ball.vx += 10;
-			}
-			// Math.random() < 0.5 ? (ball.vx += 10) : (ball.vx -= 10);
 		};
 		window.addEventListener('click', onClick);
 		return () => window.removeEventListener('click', onClick);
